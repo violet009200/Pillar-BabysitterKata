167cbd2d55312290e3ee1708d9a14d11d277b210
Bora <violet009200@gmail.com>
2019-02-18 19:11:36 -0600

    add comments

--- src/test/java/BabySitterTest.java
+++ src/test/java/BabySitterTest.java
@@ -14,6 +14,8 @@ public class BabySitterTest {
 		s = new BabySitter(11,4);
 	}
 	
+	
+	//test case for converting time
 	@Test
 	public void timeConverterTestInput5SholudReturnZero() {
 		assertEquals(0,sitter.timeConverter(5));
@@ -24,6 +26,8 @@ public class BabySitterTest {
 		assertEquals(8,sitter.timeConverter(1));
 	}
 	
+	
+	//Test for validate working hour for Baby sitter
 	@Test
 	public void validateWorkHourRangeInput4And2ShouldReturnFalse() {
 		assertEquals(false,sitter.validateWorkHourRange(4, 2));
@@ -39,11 +43,14 @@ public class BabySitterTest {
 		assertEquals(true,sitter.validateWorkHourRange(6, 3));
 	}
 	
+	
+	//Test for get hour range from start time and finish time
 	@Test
 	public void getHourRangeInput5And11ShouldReturn6() {
 		assertEquals(6,sitter.getHourRange(5, 11));
 	}
 	
+	//Test for partial payment from start time, finish time and pay rate
 	@Test
 	public void getPartialPaymentInput5And11And15ShouldReturn90() {
 		assertEquals(90,sitter.getPartialPayment(5, 11, 15));
@@ -54,29 +61,34 @@ public class BabySitterTest {
 		assertEquals(64,sitter.getPartialPayment(12, 4, 16));
 	}
 	
+	
+	//Test for get converted Start Time
 	@Test
 	public void shouldReturnConvertedStartTime() {
 		
 		assertEquals(6,s.getStartTime());
 	}
 	
+	//Test for get converted finish time
 	@Test
 	public void shouldReturnConvertedFinishTime() {
 		assertEquals(11,s.getFinishTime());
 	}
 	
+	//Test for get availability
 	@Test
 	public void shouldReturnAvailable() {
 		assertEquals(true,s.getAvailability());
 	}
 	
+	//Test for get total pay amount for Baby sitter
 	@Test
 	public void shouldgetTotalPayment() {
 		
 		assertEquals(s.getPartialPayment(11, 4, 20),s.getTotalPay());
 	}
 	
-	
+	//Test for get total pay amount for each family
 	@Test
 	public void testForFamilyA() {
 		BabySitter familyA = new BabySitter(5,4);
