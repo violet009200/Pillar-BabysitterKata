1c20bffca11f0d7e03ac7caf9bda78c611bef2dc
Bora <violet009200@gmail.com>
2019-02-18 18:35:04 -0600

    first test for time converter

--- src/main/java/BabySitter.java
+++ src/main/java/BabySitter.java
@@ -1,9 +1,22 @@
 
 public class BabySitter {
 
-	public static void main(String[] args) {
+	
+	public BabySitter() {
+		
+	}
+	
+	//set 5PM to zero 12AM to 7 4AM to 11
+	public int timeConverter(int time) {
+		if(time >= 5 && time <=12)
+			time-=5;
+
+		return time;
+	}
+	
+	/*public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+	}*/
 
 }
--- src/test/java/BabySitterTest.java
+++ src/test/java/BabySitterTest.java
@@ -1,12 +1,20 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BabySitterTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private BabySitter sitter;
+	
+	@Before
+	public void setUp(){
+		sitter = new BabySitter();
 	}
-
+	
+	@Test
+	public void timeConverterTestInput5SholudReturnZero() {
+		assertEquals(0,sitter.timeConverter(5));
+	}
+	
 }
